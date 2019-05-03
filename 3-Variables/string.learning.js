acd4c1366027ee5ea184a1247549ba7e6c471108
Devvrat Shukla <dshukla@ptc.com>
2019-05-04 00:43:09 +0530

    feat: more on strings > trimStart(), trimEnd(), trim() methods

--- 3-Variables/string.learning.js
+++ 3-Variables/string.learning.js
@@ -1,6 +1,14 @@
 const days = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday'];
 
 for (const iterator in days) {
-    day = days[iterator];
-    console.log(day.charAt(0).toUpperCase() + day.slice(1));
+  day = days[iterator];
+  console.log(day.charAt(0).toUpperCase() + day.slice(1));
 }
+
+// trimStart(), trimEnd(), trim() methods
+const someString = '       _Hello_         ';
+console.log();
+console.log('string    |', someString, '|');
+console.log('trimStart |', someString.trimStart(), '|');
+console.log('trimEnd   |', someString.trimEnd(), '|');
+console.log('trim      |', someString.trim(), '|');
