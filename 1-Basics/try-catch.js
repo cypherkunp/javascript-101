57323475544da46045fa6331d122c0dabcf8235e
Devvrat Shukla <dshukla@ptc.com>
2019-05-04 21:38:47 +0530

    feat: try-catch-finally

--- 1-Basics/try-catch.js
+++ 1-Basics/try-catch.js
@@ -4,10 +4,21 @@ try {
 } catch (err) {
   console.log(err.message);
 }
+console.log();
 
 // when you dont want to use the exception object
 try {
   console.log(notDeclaredNumber);
 } catch {
   console.log('Something went wrong...');
+}
+console.log();
+
+// finally
+try {
+  console.log(notDeclaredNumber);
+} catch {
+  console.log('caught something...');
+} finally {
+  console.log('finally something...');
 }
