3495b6442801b50f2778b65ae33d7e79bc3f1327
Devvrat Shukla <devvratshukla@gmail.com>
2019-09-07 22:21:26 +0530

    feat: more on tagget template literals

--- 9-ES6/tagged-templates.js
+++ 9-ES6/tagged-templates.js
@@ -2,26 +2,51 @@ var person = 'Mike';
 var age = 28;
 
 function myTag(strings, personExp, ageExp) {
-  var str0 = strings[0]; // "That "
-  var str1 = strings[1]; // " is a "
+    var str0 = strings[0]; // "That "
+    var str1 = strings[1]; // " is a "
 
-  // There is technically a string after
-  // the final expression (in our example),
-  // but it is empty (""), so disregard.
-  // var str2 = strings[2];
+    // There is technically a string after
+    // the final expression (in our example),
+    // but it is empty (""), so disregard.
+    // var str2 = strings[2];
 
-  var ageStr;
-  if (ageExp > 99) {
-    ageStr = 'centenarian';
-  } else {
-    ageStr = 'youngster';
-  }
+    var ageStr;
+    if (ageExp > 99) {
+        ageStr = 'centenarian';
+    } else {
+        ageStr = 'youngster';
+    }
 
-  // We can even return a string built using a template literal
-  return `${str0}${personExp}${str1}${ageStr}`;
+    // We can even return a string built using a template literal
+    return `${str0}${personExp}${str1}${ageStr}`;
 }
 
 var output = myTag`That ${person} is a ${age}`;
 
 console.log(output);
 // That Mike is a youngster
+
+function printIt() {
+    console.log('Total arguments passed > ', arguments.length);
+
+    for (const argument of arguments) {
+        console.log(argument);
+    }
+}
+
+printIt`<h1>Hello Devvrat, How are you?</h1>`;
+printIt`Hello ${'Devvrat'}, How are you?`;
+
+console.log();
+function greet() {
+    console.log('Total arguments passed > ', arguments.length);
+
+    const name = arguments[1];
+    const age = arguments[2];
+    let message = age > 18 ? 'You are eligible to vote!' : 'You are not eligible to vote';
+
+    console.log(`Hello ${name}, ${message}`);
+}
+
+greet`${'Devvrat'} ${30}`;
+greet`${'Devvrat'} ${17}`;
