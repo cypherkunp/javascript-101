e8aa60415350ad5e446f8a83c27cf273baacb4a0
Devvrat Shukla <devvratshukla@gmail.com>
2019-09-07 21:55:27 +0530

    feat: more on variadic function

--- 4-Functions/variadic.functions.js
+++ 4-Functions/variadic.functions.js
@@ -3,7 +3,7 @@
 */
 
 // Variadic function can be easily implemented using ...rest parameter
-function sum(...nums){
+function sum(...nums) {
     let total = 0;
     for (const num of nums) {
         total += num;
@@ -18,16 +18,26 @@ console.log(sum(-23, 3000, 575000));
 // Another example, calculating average of numbers
 
 function average(...numbers) {
-    let count = 0, total = 0;
+    let count = 0,
+        total = 0;
     for (const number of numbers) {
         total += number;
         count++;
     }
     count = count || 1;
-    return total/count;
+    return total / count;
 }
 
 console.log(average(2, 6));
 console.log(average(2, 3, 3, 5, 7, 10));
 console.log(average(7, 1432, 12, 13, 100));
 console.log(average());
+
+// using reduce
+
+function multiply(...params) {
+    return params.reduce((previous, current) => previous * current);
+}
+
+console.log(multiply(1, 2, 4, 5));
+console.log(multiply(2, 4, 6));
